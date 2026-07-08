6ec009f19e19e464a5776edd2a051cb371c8dea0
xiaoke <xiaoke@outlook.com>
2026-07-08 23:04:37 +0800

    修复点击更多-文章的404

--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -9,8 +9,8 @@ export const zhNavbar = navbar([
     icon: "info",
     prefix: "/",
     children: [
-      { text: "文章", icon: "note", link: "docs" },
-      { text: "关于", icon: "info", link: "about" },
+      { text: "文章", icon: "note", link: "/docs/" },
+      { text: "关于", icon: "info", link: "/about.html" },
     ],
   }
 ]);
